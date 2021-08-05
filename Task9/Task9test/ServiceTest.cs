c2e85f234f95aedf6300e30bc0476ff888c7b467
EnErgynEEr <hello.mr.sanders@gmail.com>
2021-08-05 23:26:04 +0500

    Ver4. Added services tests

---
+++ Task9/Task9test/ServiceTest.cs
@@ -0,0 +1,113 @@
+﻿using DataAccess.ViewModel;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using NUnit.Framework;
+using Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebAPI;
+
+namespace Task9test
+{
+    [TestFixture]
+    public class ServiceTest
+    {
+        private ICourseService CourseService;
+        private IGroupService GroupService;
+        private IStudentService StudentService;
+        private CourseViewModel CourseView;
+        private GroupViewModel GroupView;
+        private StudentViewModel StudentView;
+
+        [SetUp]
+        public void Init()
+        {
+            TestServer testServer = new TestServer(
+                new WebHostBuilder().ConfigureAppConfiguration(_ => { }).UseStartup<Startup>());
+            CourseService = testServer.Services.GetService(typeof(ICourseService)) as ICourseService;
+            GroupService = testServer.Services.GetService(typeof(IGroupService)) as IGroupService;
+            StudentService = testServer.Services.GetService(typeof(IStudentService)) as IStudentService;
+        }
+
+        [Test, Order(1)]
+        public void TestCreateCourse()
+        {
+            CourseViewModel courseViewModel = ViewModelBuilder.GetCourse();
+            CourseService.InsertCourse(courseViewModel);
+            CourseView = CourseService.GetCourse().LastOrDefault();
+            Assert.AreEqual(courseViewModel.CourseName, CourseView.CourseName);
+        }
+
+        [Test, Order(2)]
+        public void TestEditCourse()
+        {
+            string editCourseName = "TestNameEdit2";
+            CourseView.CourseName = editCourseName;
+            CourseService.UpdateCourse(CourseView);
+            CourseView = CourseService.GetCourse().LastOrDefault();
+            Assert.AreEqual(editCourseName, CourseView.CourseName);
+        }
+
+        [Test, Order(3)]
+        public void TestCreateGroup()
+        {
+            GroupViewModel groupViewModel = ViewModelBuilder.GetGroup(CourseView.Id);
+            GroupService.InsertGroup(groupViewModel);
+            GroupView = GroupService.GetGroup().LastOrDefault();
+            Assert.AreEqual(groupViewModel.GroupName, GroupView.GroupName);
+        }
+
+        [Test, Order(4)]
+        public void TestEditGroup()
+        {
+            string editGroupName = "TestGroupEdit2";
+            GroupView.GroupName = editGroupName;
+            GroupService.UpdateGroup(GroupView);
+            GroupView = GroupService.GetGroup().LastOrDefault();
+            Assert.AreEqual(editGroupName, GroupView.GroupName);
+        }
+
+        [Test, Order(5)]
+        public void TestCreateStudent()
+        {
+            StudentViewModel studentViewModel = ViewModelBuilder.GetStudent(GroupView.Id);
+            StudentService.InsertStudent(studentViewModel);
+            StudentView = StudentService.GetStudent().LastOrDefault();
+            Assert.AreEqual(studentViewModel.FirstName, StudentView.FirstName);
+        }
+
+        [Test, Order(6)]
+        public void TestEditStudent()
+        {
+            string editStudentFirstName = "TestStudentEdit2";
+            StudentView.FirstName = editStudentFirstName;
+            StudentService.UpdateStudent(StudentView);
+            StudentView = StudentService.GetStudent().LastOrDefault();
+            Assert.AreEqual(editStudentFirstName, StudentView.FirstName);
+        }
+
+        [Test, Order(7)]
+        public void TestDeleteStudent()
+        {
+            StudentService.DeleteStudent(StudentView.Id);
+            Assert.Throws<NullReferenceException>(() => StudentService.GetStudent(StudentView.Id));
+        }
+
+        [Test, Order(8)]
+        public void TestDeleteGroup()
+        {
+            GroupService.DeleteGroup(GroupView.Id);
+            Assert.Throws<NullReferenceException>(() => GroupService.GetGroup(GroupView.Id));
+        }
+
+        [Test, Order(9)]
+        public void DeleteTest()
+        {
+            CourseService.DeleteCourse(CourseView.Id);
+            Assert.Throws<NullReferenceException>(() => CourseService.GetCourse(CourseView.Id));
+        }
+    }
+}
---
+++ Task9/Task9test/ViewModelBuilder.cs
@@ -0,0 +1,37 @@
+﻿using DataAccess.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task9test
+{
+    public class ViewModelBuilder
+    {
+        public static CourseViewModel GetCourse()
+        {
+            CourseViewModel courseView = new CourseViewModel();
+            courseView.CourseName = "TestCourseName";
+            courseView.CourseDesc = "TestCourseDescription";
+            return courseView;
+        }
+
+        public static GroupViewModel GetGroup(int courseId)
+        {
+            GroupViewModel groupView = new GroupViewModel();
+            groupView.GroupName = "TestGroupName";
+            groupView.CourseId = courseId;
+            return groupView;
+        }
+
+        public static StudentViewModel GetStudent(int groupId)
+        {
+            StudentViewModel studentView = new StudentViewModel();
+            studentView.FirstName = "TestFirstName";
+            studentView.LastName = "TestLastName";
+            studentView.GroupId = groupId;
+            return studentView;
+        }
+    }
+}
